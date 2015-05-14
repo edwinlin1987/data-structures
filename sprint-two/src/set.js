8e8decddedbec71ac09b29780ae1536835825c3e
edwinlin1987 <edwinlin1987@gmail.com>
2015-05-14 16:39:11 -0700

    Completed Set Object

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,25 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  //this._storage.push(item);
+  this[item] = item;
 };
 
 setPrototype.contains = function(item){
+  // for (var i = 0; i < this._storage.length; i++) {
+  //   if (this._storage[i] === item) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+  return !!this[item];
+
 };
 
 setPrototype.remove = function(item){
+  if (this.hasOwnProperty(item)) {
+    delete this[item];
+  }
 };
 
 /*
