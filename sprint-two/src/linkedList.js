85c2c2c90157dc68f84b1760cb9bcf6df563d239
edwinlin1987 <edwinlin1987@gmail.com>
2015-05-14 11:17:24 -0700

    Completed linkedList object, passes all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.head === null){
+      list.head = Node(value);
+      list.tail = list.head;
+    } else{
+      var temp = Node(value);
+      list.tail.next = temp;
+      list.tail = temp;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head !== null){
+      var temp = list.head.value;
+      list.head = list.head.next;
+      return temp;
+    }
   };
 
   list.contains = function(target){
+    if (list.tail === null) {
+      return false;
+    }
+    //reach into head
+    if (list.head.value === target) {
+      return true;
+    } else {
+      var temp = list.head.next;
+      while (temp !== null) {
+        if (temp.value === target) {
+          return true;
+        } else {
+          temp = temp.next;
+        }
+      }
+
+    }
+
+    return false;
+      //grab head's next property
+      //return true if it finds target, otherwise continue loop
+        // if never found, return false;
   };
 
   return list;
