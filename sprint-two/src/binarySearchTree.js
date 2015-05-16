1ff4d7789151f7bc04e217e010991d2e2f727f87
edwinlin1987 <edwinlin1987@gmail.com>
2015-05-15 18:21:14 -0700

    Completed value insertion of binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,35 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTreeMethods);
+  tree.value = value;
+  return tree;
 };
+
+var BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function (value) {
+  if (this.value > value) {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTreeMethods.contains = function () {
+
+};
+
+BinarySearchTreeMethods.depthFirstLog = function () {
+
+};
+
 
 
 /*
